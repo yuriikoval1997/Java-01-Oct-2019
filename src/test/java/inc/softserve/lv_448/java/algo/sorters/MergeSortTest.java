e86e118470b84d2ea221d56679b264249e099889
yuriikoval1997 <yuriikoval1997@gmail.com>
2019-10-03 18:07:43 +0300

    refactoring

--- src/test/java/inc/softserve/lv_448/java/algo/sorters/MergeSortTest.java
+++ src/test/java/inc/softserve/lv_448/java/algo/sorters/MergeSortTest.java
@@ -1,15 +1,10 @@
 package inc.softserve.lv_448.java.algo.sorters;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class MergeSortTest extends SortTest {
 
--- src/test/java/inc/softserve/lv_448/java/algo/sorters/QuickSortTest.java
+++ src/test/java/inc/softserve/lv_448/java/algo/sorters/QuickSortTest.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class QuickSortTest extends SortTest {
