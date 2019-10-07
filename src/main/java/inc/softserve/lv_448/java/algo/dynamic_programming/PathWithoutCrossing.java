e2a50eaeb3287cfb2bfba1b3fb92345128e73d54
user <w.turko3000@gmail.com>
2019-10-07 15:19:05 +0300

    Add commit PathWithoutCrossing.

--- src/main/java/inc/softserve/lv_448/java/algo/dynamic_programming/PathWithoutCrossing.java
+++ src/main/java/inc/softserve/lv_448/java/algo/dynamic_programming/PathWithoutCrossing.java
@@ -3,16 +3,27 @@ package inc.softserve.lv_448.java.algo.dynamic_programming;
 import inc.softserve.lv_448.java.algo.starter.Startable;
 import inc.softserve.lv_448.java.algo.utils.ConsoleReader;
 
-import java.util.Scanner;
-
+/**
+ * Class for calculation non-crossing lines to connect points in a circle.
+ *
+ * @author Volodymyr Turko
+ */
 public class PathWithoutCrossing implements Startable {
 
-
+    /**
+     * Main method for getting input, calculating and output of result.
+     */
     @Override
     public void start(ConsoleReader consoleReader){
         System.out.println("Your result is: " + calculate(consoleReader.readInt("Enter even number")));
     }
 
+    /**
+     * Method for calculating the number of ways.
+     *
+     * @param pointCount The number of points.
+     * @return The number of ways to connect point without crossing.
+     */
     public int calculate(int pointCount){
         if (pointCount % 2 != 0 || pointCount < 1) {
             return pointCount = 0;
