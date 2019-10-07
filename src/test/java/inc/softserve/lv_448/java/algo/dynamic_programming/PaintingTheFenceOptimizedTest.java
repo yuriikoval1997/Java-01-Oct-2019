029fb4234e2a794d98e1b8b2c30f9f68ecab1eb9
nomadness <vitalii.skolozdra@gmail.com>
2019-10-07 20:56:37 +0300

    Add tests for PaintingTheFenceOptimized

---
+++ src/test/java/inc/softserve/lv_448/java/algo/dynamic_programming/PaintingTheFenceOptimizedTest.java
@@ -0,0 +1,41 @@
+package inc.softserve.lv_448.java.algo.dynamic_programming;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PaintingTheFenceOptimizedTest {
+
+    private PaintingTheFenceOptimized testClass;
+
+    @BeforeEach
+    void setUp() {
+        testClass = new PaintingTheFenceOptimized();
+    }
+
+    @Test
+    void paintingTheFenceOptimized3And2() {
+        long actual;
+        int expected = 6;
+
+        int n = 3;
+        int k = 2;
+
+        actual = testClass.countWays(n, k);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void paintingTheFenceOptimized2And4() {
+        long actual;
+        int expected = 16;
+
+        int n = 2;
+        int k = 4;
+
+        actual = testClass.countWays(n, k);
+        assertEquals(expected, actual);
+    }
+
+}
