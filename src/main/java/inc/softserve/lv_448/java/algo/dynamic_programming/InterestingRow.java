d54c4acd9b795327b2bf1a581cf57679fbc377b8
Bogdan Kuzenko <kuzenko.b@outlook.com>
2019-10-07 22:45:16 +0300

    Add InterestingRow

---
+++ src/main/java/inc/softserve/lv_448/java/algo/dynamic_programming/InterestingRow.java
@@ -0,0 +1,43 @@
+package inc.softserve.lv_448.java.algo.dynamic_programming;
+
+import inc.softserve.lv_448.java.algo.starter.Startable;
+import inc.softserve.lv_448.java.algo.utils.ConsoleUtil;
+
+/**
+ * That class find which is the member of position input integer
+ * in our interesting row
+ */
+public class InterestingRow implements Startable {
+
+    /**
+     * Method starts task flow execution
+     */
+    @Override
+    public void start(ConsoleUtil consoleUtil) {
+        consoleUtil.printResult(findInterestingRowMember(consoleUtil
+                .readInt("Please enter position member of integers row: ")));
+    }
+
+    /**
+     * Performs main calculations.
+     * Makes interesting row of integers array and find
+     * positions member in row of input userNumber
+     *
+     * @param userNumber value entered by user
+     * @return number in array at position userNumber.
+     */
+    public int findInterestingRowMember(int userNumber) {
+
+        if (userNumber < 1) {
+            return 0;
+        }
+        int[] interestingRowArray = new int[userNumber + 2];
+        interestingRowArray[1] = 1;
+        interestingRowArray[2] = 1;
+        for (int i = 3; i <= userNumber; i++) {
+            interestingRowArray[i] = 2 * interestingRowArray[i - 2] + interestingRowArray[i - 1];
+        }
+        return interestingRowArray[userNumber];
+    }
+
+}
