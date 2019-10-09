01cfaad510bbcaac69bb456e575f620dfc3ee26a
nomadness <vitalii.skolozdra@gmail.com>
2019-10-09 11:15:08 +0300

    Update comments in WaysToTileTheFloor

--- src/main/java/inc/softserve/lv_448/java/algo/dynamic_programming/WaysToTileTheFloor.java
+++ src/main/java/inc/softserve/lv_448/java/algo/dynamic_programming/WaysToTileTheFloor.java
@@ -31,27 +31,24 @@ public class WaysToTileTheFloor implements Startable {
      * @return count[n] number of ways to tile the floor
      */
     public int countWays(int n, int m) {
-        // table to store values
-        // of subproblems
+        // Table of subproblems
         int count[] = new int[n + 1];
         count[0] = 0;
 
         // Fill the table upto value n
-        int i;
-        for (i = 1; i <= n; i++) {
-            // recurrence relation
+        for (int i = 1; i <= n; i++) {
+            // Recurrence relation
             if (i > m) {
                 count[i] = count[i - 1] + count[i - m];
             } else if (i < m) {
                 // base cases
                 count[i] = 1;
             } else {
-                // i = = m
+                // i == m
                 count[i] = 2;
             }
         }
 
-        // required number of ways
         return count[n];
     }
 
